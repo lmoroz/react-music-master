f384d84db4e950612e690a61cd4296abb7aa4e48
Larisa Moroz <larisa.moroz@gmail.com>
2017-02-26 16:40:51 +0800

    Profile and Track components shortened to simple funcs

--- src/Profile.js
+++ src/Profile.js
@@ -1,61 +1,31 @@
 import React from 'react';
-import './App.css';
 
-class Profile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-  // propTypes: {
-  //   artist: React.PropTypes.Object.isRequired,
-  // }
-    render() {
-        const artist =
-            this.props.artist
-            || {followers: { href: null, total: 2521554 },
-                genres: ['alternative metal', 'alternative rock', 'garage rock', 'indie rock', 'permanent wave', 'piano rock', 'post-grunge', 'rock'],
-                href: 'https://api.spotify.com/v1/artists/12Chz98pHFMPJEknJQMWvI',
-                id: '12Chz98pHFMPJEknJQMWvI',
-                images: [{
-                    height: 133,
-                    url: 'https://i.scdn.co/image/837c977024362a7f6d1873027e2a8664e21f911a',
-                    width: 200}
-                ],
-                name: 'Muse',
-                popularity: 79,
-                type: 'artist',
-                uri: 'spotify:artist:12Chz98pHFMPJEknJQMWvI'
-            };
-        return (
-            <div className="profile">
-                <img
-                    alt="Profile"
-                    src={artist.images[artist.images.length - 1].url} className="profile-img"
-                />
-                <div className="profile-info">
-                    <div className="profile-name">{artist.name}</div>
-                    <div className="profile-followers">
-                        {artist.followers.total} followers
-                    </div>
-                    <div className="profile-genres">
-                        {
-                            artist.genres.map((genre, key) => {
-                                const genrestring = (key === (artist.genres.length - 1)) ?
-                                    `${genre}` : `${genre}, `;
-                                return (
-                                    <span key={key}>{genrestring}</span>
-                                );
-                            })
-                        }
-                    </div>
-                </div>
+const Profile = ({artist}) => (
+    <div className="profile">
+        <img
+            alt="Profile"
+            src={artist.images[artist.images.length - 1].url} className="profile-img"
+        />
+        <div className="profile-info">
+            <div className="profile-name">{artist.name}</div>
+            <div className="profile-followers">
+                {artist.followers.total} followers
             </div>
-        );
-    }
-}
+            <div className="profile-genres">
+                {
+                    artist.genres.map((genre, key) => {
+                        const genrestring = (key === (artist.genres.length - 1)) ?
+                            `${genre}` : `${genre}, `;
+                        return (
+                            <span key={key}>{genrestring}</span>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    </div>
+);
+
 Profile.propTypes = {
     artist: React.PropTypes.object,
 };
--- src/Track.js
+++ src/Track.js
@@ -1,46 +1,34 @@
 import React from 'react';
 
-class Track extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-
-    render () {
-        return (
-            <div
-                className={`track ${this.props.active}`}
-                onClick={this.props.onClick}
-            >
-                <img
-                    src={this.props.track.album.images[0].url}
-                    alt={this.props.track.name}
-                    title={this.props.track.name}
-                />
-                <div className="track-title">
-                    <div className="track-name">{this.props.track.name}</div>
-                    from <span className="album-name">{this.props.track.album.name}</span>
-                </div>
-                <div className="track-toggle">
-                    <div className="play">
-                        &#9654;
-                    </div>
-                    <div className="pause">
-                        | |
-                    </div>
-                </div>
+const Track = ({onClick, track, active}) => (
+    <div
+        className={`track ${active}`}
+        onClick={onClick}
+    >
+        <img
+            src={track.album.images[0].url}
+            alt={track.name}
+            title={track.name}
+        />
+        <div className="track-title">
+            <div className="track-name">{track.name}</div>
+            from <span className="album-name">{track.album.name}</span>
+        </div>
+        <div className="track-toggle">
+            <div className="play">
+                &#9654;
             </div>
-        );
-    }
-}
+            <div className="pause">
+                | |
+            </div>
+        </div>
+    </div>
+);
 
 Track.propTypes = {
-    track: React.PropTypes.object,
     active: React.PropTypes.string,
     onClick: React.PropTypes.func,
+    track: React.PropTypes.object,
 };
 
 export default Track;
